6989e8831a7078e3027827d6e9b90909bf69458a
Glich_Limbo <63367591+HryhorenkoVitalii@users.noreply.github.com>
2020-09-19 17:32:52 +0300

    normalize_headers (done) fix name

--- src/utils/normalizeHeaders.py
+++ src/utils/normalizeHeaders.py
@@ -1,10 +1,27 @@
 def normalize_headers(headers: dict) -> dict:
-    result = {}
+    normalized_headers = {}
     for header in headers:
         if "-" in header:
             key_list = [x.capitalize() for x in header.split("-")]
             key = "-".join(key_list)
-            result[key] = headers[header]
+            normalized_headers[key] = headers[header]
         else:
-            result[header.capitalize()] = headers[header]
-    return result
+            normalized_headers[header.capitalize()] = headers[header]
+    return normalized_headers
+
+
+g = {
+    "ACCEPT": "*/*",
+    "Upgrade-insecure-Requests": "1",
+    "accept-encoding": "gzip, deflate, br",
+    "Extra-Header-1": "test",
+    "Cookie": "foo=bar",
+    "Host": "www.example.com",
+    "referer": "https://www.google.com/",
+    "Extra-Header-2": "test",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "user-agent": "<USER_AGENT>",
+    "Connection": "keep-alive",
+}
+
+print(normalize_headers(g))
